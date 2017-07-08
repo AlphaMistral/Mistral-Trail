2d96753dac17bde9f4efa3df4b3e8a4291a69426
Jingping Yu <Mistral@weymire.com>
2017-07-08 11:03:44 +0800

    RingBuffer Attached. Now moving to another branch.

---
+++ Assets/Mistral Trail/Test Area/Scripts/Core/TrailBase.cs
@@ -0,0 +1,96 @@
+﻿/*
+ Author: Jingping Yu
+ RTX: joshuayu
+ Created on: 2017/07/08
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mistral.Effects.Trail
+{
+    /// <summary>
+    /// This Serializable Class Serves as a Parameter Storage for Trail Rendering. 
+    /// </summary>
+    [System.Serializable]
+    public class TrailParameter
+    {
+        public Material trailMaterial;
+        public float lifeTime;
+        public AnimationCurve sizeOverLife = new AnimationCurve();
+        public Gradient colorOverLife;
+    }
+
+	/// <summary>
+	/// This Class Represents a Virtual Point on the Trail. 
+	/// </summary>
+	public class TrailPoint
+	{
+		/// <summary>
+		/// The Looking-at direction. 
+		/// Namely the direction towards which this point is to move in the next frame :)
+		/// </summary>
+		public Vector3 forwardDirection;
+
+		/// <summary>
+		/// The Virtual Position of this TrailPoint.
+		/// Please note that a TrailPoint Could not be a GameObject.
+		/// Hence, its position should be virtual rather than transform.position. 
+		/// </summary>
+		public Vector3 position;
+
+		public int index;
+
+		public float timeSoFar = 0;
+		public float distance2Src = 0;
+
+		/// <summary>
+		/// DISCUSSION: Whether deltaTime should be provided or not. 
+		/// HYPOTHESIS: Time.deltaTime is not an O(1) operation. 
+		/// </summary>
+		/// <param name="deltaTime"></param>
+		public virtual void Update(float deltaTime)
+		{
+			timeSoFar += deltaTime;
+		}
+	}
+
+	public class TrailGraphics : IDisposable
+	{
+
+		public void Dispose()
+		{
+
+		}
+	}
+
+    /// <summary>
+    /// Abstract Class for all kinds of trails. 
+    /// It also simultaneously serves as the Trail Rendering Manager! 
+    /// Specifically, take the "LateUpdate" function for reference. 
+    /// </summary>
+    public abstract class TrailBase : MonoBehaviour
+    {
+        #region Public Variables
+
+        public TrailParameter parameter;
+        public bool Emit = false;
+
+		#endregion
+
+		#region Protected Variables
+
+		protected bool isEmitting;
+
+		#endregion
+
+		#region Private Static Variables (Trail Rendering Manager)
+
+		//private static Dictionary<Material, List<>> mat2Trail;
+
+		#endregion
+
+	}
+}
---
+++ Assets/Mistral Trail/Test Area/Scripts/Utility/DataStructure/RingBuffer.cs
@@ -0,0 +1,208 @@
+﻿/*
+ Author: Jingping Yu
+ RTX: joshuayu
+ Created on: 2017/07/08
+
+ The following code is copied and revised from somewhere else.
+ Don't fucking temper with it! 
+ */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RingBuffer<T> : IEnumerable, IEnumerable<T>, ICollection<T>, IList<T>
+{
+	#region Public Variables
+
+	public int Capacity { get; private set; }
+	public int Count { get; private set; }
+
+	#endregion
+
+	#region Private Variables
+
+	private T[] buffer;
+	private int index;
+	private long version;
+
+	#endregion
+
+	#region ctor
+
+	public RingBuffer(int capacity)
+	{
+		if (capacity > 0)
+		{
+			Capacity = capacity;
+			buffer = new T[capacity];
+		}
+		else
+		{
+			throw new ArgumentException("RingBuffer的容量不能小于等于0!");
+		}
+	}
+
+	#endregion
+
+	#region RingBuffer Interfaces
+
+	public void Add(T item)
+	{
+		buffer[index++ % Capacity] = item;
+		if (Count < Capacity) Count++;
+		version++;
+	}
+
+	public void Clear()
+	{
+		for (int i = 0; i < Count; i++)
+		{
+			buffer[i] = default(T);
+		}
+		index = 0;
+		Count = 0;
+		//Be aware that the version is never cleared to 0 ... 
+		version++;
+	}
+
+	public bool Contains(T item)
+	{
+		return IndexOf(item) >= 0;
+	}
+
+	/// <summary>
+	/// Don't you Ever query IndexOf(NULL). 
+	/// Otherwise I will hate you.
+	/// Trust me, it won't work! 
+	/// </summary>
+	/// <param name="item"></param>
+	/// <returns></returns>
+	public int IndexOf(T item)
+	{
+		/// 2 b continued ... 
+		if (item == null)
+		{
+
+		}
+		for (int i = 0; i < Count; i++)
+		{
+			T temp = buffer[(index - Count + i) % Capacity];
+			if (item != null && item.Equals(temp))
+				return i;
+		}
+		return -1;
+	}
+
+	/// <summary>
+	/// Copies the whole Ring Buffer to an array. 
+	/// However if the array's capacity is too small ... 
+	/// I dunno what 2 do. (i am lazy :( )
+	/// </summary>
+	/// <param name="target"></param>
+	/// <param name="startIndex"></param>
+	public void CopyTo(T[] target, int startIndex)
+	{
+		for (int i = 0; i < Count; i++)
+		{
+			target[i + startIndex] = buffer[(index - Count + i) % Capacity];
+		}
+	}
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		long storedVersion = version;
+		for (int i = 0; i < Count; i++)
+		{
+			if (version != storedVersion)
+			{
+				throw new InvalidOperationException("Ring Buffer 被外部修改, 退出!");
+			}
+			yield return this[i];
+		}
+	}
+
+	public T this[int idx]
+	{
+		get
+		{
+			return buffer[ (index - Count + idx) % Capacity ];
+		}
+		set
+		{
+			
+		}
+	}
+
+	public void Insert(int idx, T item)
+	{
+		if (idx < 0 || idx > Count)
+		{
+			throw new IndexOutOfRangeException();
+		}
+
+		if (index == Count)
+		{
+			Add(item);
+			return;
+		}
+
+		int count = Math.Min(Count, Capacity - 1) - idx;
+
+		int id = (index - Count + idx) % Capacity;
+
+		for (int i = id + count; i > id; i--)
+		{
+			buffer[i % Capacity] = buffer[ (i - 1) % Capacity ];
+		}
+
+		buffer[id] = item;
+		if (Count < Capacity)
+		{
+			Count++;
+			index++;
+		}
+
+		version++;
+	}
+
+	public bool Remove(T item)
+	{
+		int position = IndexOf(item);
+		if (index < 0)
+			return false;
+		RemoveAt(position);
+		return true;
+	}
+
+	public void RemoveAt(int idx)
+	{
+		if (idx < 0 || idx > Count)
+		{
+			throw new IndexOutOfRangeException();
+		}
+		for (int i = idx; i < Count - 1; i++)
+		{
+			buffer[(index - Count + i) % Capacity] = buffer[ (index - Count + i + 1) % Capacity ];
+		}
+		buffer[(index - 1) % Capacity] = default(T);
+		index--;
+		Count--;
+		version++;
+	}
+
+	/// <summary>
+	/// Why did I implement this? 
+	/// Holy something and son of someone. 
+	/// </summary>
+	bool ICollection<T>.IsReadOnly { get { return false; } }
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return this.GetEnumerator();
+	}
+
+	#endregion
+
+}
