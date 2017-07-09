953921a7ee97c8903996d2ad8e727f60700718be
AlphaMistral <Mistral@weymire.com>
2017-07-09 16:48:03 +0800

    Smooth Trail is basically ready.

--- Assets/Mistral Trail/Test Area/Scripts/HL/SmoothTrail.cs
+++ Assets/Mistral Trail/Test Area/Scripts/HL/SmoothTrail.cs
@@ -2,7 +2,7 @@
  ### Mistral Trail System ###
  Author: Jingping Yu
  RTX: joshuayu
- Created on: 2017/07/08
+ Created on: 2017/07/09
  */
 
 using System.Collections;
@@ -27,8 +27,8 @@ namespace Mistral.Effects.Trail
 		/// </summary>
 		public class AdditionalPoint
 		{
-			Vector3 position;
-			Vector3 forward;
+			public Vector3 position;
+			public Vector3 forward;
 		}
 
 		#endregion
@@ -43,6 +43,11 @@ namespace Mistral.Effects.Trail
 		/// </summary>
 		public int maxPointNumber = 15;
 
+		/// <summary>
+		/// Number of points to be inserted between two control points. 
+		/// </summary>
+		public int pointsInMiddle;
+
 		#endregion
 
 		#region Private Variables
@@ -55,24 +60,140 @@ namespace Mistral.Effects.Trail
 
 		#region MonoBehaviours
 
-		protected void Start()
+		protected override void Start()
 		{
 			base.Start();
 			lastPosition = m_transform.position;
 		}
 
-		protected void Update()
+		protected override void Update()
 		{
-			
+			if (isEmitting)
+			{
+				distanceMoved += Vector3.Distance(m_transform.position, lastPosition);
+				if (!Mathf.Approximately(distanceMoved, 0.0f) && distanceMoved >= minVertexDistance)
+				{
+					AddControlPoint(lastPosition);
+					distanceMoved = 0.0f;
+				}
+				else
+				{
+					controlPoints[controlPoints.Count - 1].position = m_transform.position;
+					///Forward Override goes here
+				}
+				lastPosition = m_transform.position;
+			}
+
+			base.Update();
 		}
 
 		#endregion
 
 		#region Override Methods
 
+		protected override void OnStartEmit()
+		{
+			lastPosition = m_transform.position;
+			distanceMoved = 0.0f;
+			controlPoints = new RingBuffer<AdditionalPoint>(maxPointNumber);
+			controlPoints.Add(new AdditionalPoint { position = lastPosition });
+			///Forward Override goes here
+			AddPoint(new TrailPoint(), lastPosition);
+			AddControlPoint(lastPosition);
+		}
+
 		protected override int GetMaxPoints()
 		{
 			return maxPointNumber;
+		}
+
+		protected override void UpdateTrail(TrailGraphics trail, float deltaTime)
+		{
+			if (!trail.activeSelf)
+				return;
+
+			int trailPointIdx = 0;
+			for (int i = 0; i < controlPoints.Count; i++)
+			{
+				trail.points[trailPointIdx].position = controlPoints[i].position;
+				///Forward Override goes here
+				trailPointIdx++;
+				if (i < controlPoints.Count - 1)
+				{
+					Vector3 cp1, cp2;
+					float distance = Vector3.Distance(controlPoints[i].position, controlPoints[i + 1].position) / 2;
+					if (i == 0)
+					{
+						cp1 = controlPoints[i].position + (controlPoints[i + 1].position - controlPoints[i].position).normalized * distance;
+					}
+					else
+					{
+						cp1 = controlPoints[i].position + (controlPoints[i + 1].position - controlPoints[i - 1].position).normalized * distance;
+					}
+
+					int nextIdx = i + 1;
+
+					if (nextIdx == controlPoints.Count - 1)
+					{
+						cp2 = controlPoints[nextIdx].position + (controlPoints[nextIdx - 1].position - controlPoints[nextIdx].position).normalized * distance;
+					}
+					else
+					{
+						cp2 = controlPoints[nextIdx].position + (controlPoints[nextIdx - 1].position - controlPoints[nextIdx + 1].position).normalized * distance;
+					}
+
+					TrailPoint current = trail.points[trailPointIdx - 1];
+					TrailPoint next = trail.points[trailPointIdx + pointsInMiddle];
+
+					for (int j = 0; j < pointsInMiddle; j++)
+					{
+						float t = ( ( (float)j + 1 ) / ( (float)pointsInMiddle + 1 ) );
+						trail.points[trailPointIdx].position = Bezier(t, controlPoints[i].position, cp1, controlPoints[i + 1].position, cp2);
+						trail.points[trailPointIdx].timeSoFar = Mathf.Lerp(current.timeSoFar, next.timeSoFar, t);
+
+						///Forward goes here. 
+						trailPointIdx++;
+					}
+				}
+			}
+			int lastCPIdx = (pointsInMiddle + 1) * (controlPoints.Count - 1);
+			int prevCPIdx = lastCPIdx - pointsInMiddle - 1;
+			int activeCount = lastCPIdx + 1;
+
+			float distance2Src = trail.points[prevCPIdx].distance2Src;
+			for (int i = prevCPIdx + 1; i < activeCount; i++)
+			{
+				distance2Src += Vector3.Distance(trail.points[i - 1].position, trail.points[i].position);
+				trail.points[i].distance2Src = distance2Src;
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void AddControlPoint(Vector3 position)
+		{
+			for (int i = 0; i < pointsInMiddle; i++)
+			{
+				AddPoint(new TrailPoint(), position);
+			}
+			AddPoint(new TrailPoint(), position);
+			AdditionalPoint ap = new AdditionalPoint { position = position };
+			///Forward goes here. 
+			controlPoints.Add(ap);
+
+		}
+
+		private Vector3 Bezier(float t, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+		{
+			float u = 1 - t;
+			float tt = t * t;
+			float uu = u * u;
+			float uuu = uu * u;
+			float ttt = tt * t;
+
+			return uuu * p1 + 3 * uu * t * p2 + 3 * u * tt * p3 + ttt * p4;
 		}
 
 		#endregion
