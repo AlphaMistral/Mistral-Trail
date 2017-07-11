19e02ba76e0c08a1adc42f875a697301f046a185
AlphaMistral <Mistral@weymire.com>
2017-07-11 14:55:58 +0800

    Mouse Follower Commented.
    AT.

--- Assets/Mistral Trail/Scripts/ShowCase/MouseFollower.cs
+++ Assets/Mistral Trail/Scripts/ShowCase/MouseFollower.cs
@@ -1,4 +1,4 @@
-﻿using PigeonCoopToolkit.Effects.Trails;
+﻿//using PigeonCoopToolkit.Effects.Trails;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,7 +7,7 @@ using System.Linq;
 public class MouseFollower : MonoBehaviour
 {
 
-    public List<TrailRenderer_Base> Trails;
+    //public List<TrailRenderer_Base> Trails;
 
 
 	// Use this for initialization
@@ -19,14 +19,14 @@ public class MouseFollower : MonoBehaviour
 	void Update () {
 	    if (Input.GetMouseButton(0))
 	    {
-            Trails.ForEach(a => a.Emit = true);
+            //Trails.ForEach(a => a.Emit = true);
 
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 0.01f));
 
 	    }
 	    else
 	    {
-            Trails.ForEach(a => a.Emit = false);
+            //Trails.ForEach(a => a.Emit = false);
 	    }
 	}
 
